86a1ea1c7c57ad9919e63373fd99b5a18e2fe432
ramakrishnan <admin@jseeng.com>
2025-09-06 18:50:02 +0530

    UI finalized - Clearance system with lock/unlock, default values, and smart MEP dropdown

--- obj/Debug/JSE_RevitAddin_MEP_OPENINGS.AssemblyInfo.cs
+++ obj/Debug/JSE_RevitAddin_MEP_OPENINGS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("OPENING PLACEMENT")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+260e173dfba654b567662d89ae43c9c656f03cb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+868b2442c5524bb80043d9de9e2a948ce7441337")]
 [assembly: System.Reflection.AssemblyProductAttribute("JSE_RevitAddin_MEP_OPENINGS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JSE_RevitAddin_MEP_OPENINGS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
