e0f002943d2dd456a5621801214d73d3adc5ccb0
ramakrishnan <admin@jseeng.com>
2025-11-19 17:32:13 +0530

    fix: excluded _BACKUPS folder from project compilation - prevents backup files from causing build errors

--- obj/Debug R21/JSE_RevitAddin_MEP_OPENINGS.AssemblyInfo.cs
+++ obj/Debug R21/JSE_RevitAddin_MEP_OPENINGS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug R21")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("OPENING PLACEMENT")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf5cce69eb043dd017c84420d1c1f850618aaa8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3f5924a2ba3e31633ce0e416bc7bf20479ac85c")]
 [assembly: System.Reflection.AssemblyProductAttribute("JSE_RevitAddin_MEP_OPENINGS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JSE_RevitAddin_MEP_OPENINGS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
